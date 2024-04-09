10841c3a89e5be482f3153511ec546a3fe135198
Onkar31 <onkarbhogle08@gmail.com>
2024-04-09 21:15:21 +0530

    Zero Values

---
+++ 002.Data-types-and-Variables/04_zero-values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message string
+	var isTrue bool
+	var num int
+
+	// Default values of variables of different data types
+	fmt.Println(message)
+	fmt.Println(isTrue)
+	fmt.Println(num)
+}
