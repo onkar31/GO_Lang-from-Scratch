f957ac4f4bc92525813d05f4ee6f5196c24e0326
Onkar31 <onkarbhogle08@gmail.com>
2024-04-13 20:11:05 +0530

    character and string operations

---
+++ 002.Data-types-and-Variables/09_character-strings-operations/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var ch rune = '5'
+	fmt.Println("Value of ch:", ch)
+
+	// Character have the same properties as whole numbers because they are whole number in memory
+	// All oeprations on characters work the same way they do on whole numbers
+	ch = ch + 1
+	fmt.Println(ch)
+	fmt.Println(ch / 2)
+	fmt.Println(ch * 2)
+	fmt.Println(ch % 2)
+	fmt.Println("+++++++++++++++string operations+++++++++++++++++")
+
+	// The only operation for string available is called "concatenation"
+	// It allows us to "glue" two strings together
+	var message string = "Go is "
+	message = message + "awesome!" //concatenation
+	fmt.Println(message)
+
+}
