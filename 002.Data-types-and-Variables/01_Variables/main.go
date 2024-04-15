b4fd4062f624f3abab71e3e71dbdb7bf10623291
Onkar31 <onkarbhogle08@gmail.com>
2024-04-15 08:46:12 +0530

    Variable Naming & Naming conventions

--- 002.Data-types-and-Variables/01_Variables/main.go
+++ 002.Data-types-and-Variables/01_Variables/main.go
@@ -20,4 +20,17 @@ func main() {
 	// Although I'm executing the same instruction
 	// the value stored in "message is different"
 	fmt.Println(message)
+
+	// what makes a valid variable name?
+	// characters from a-z (capital case as well)
+	// numbers (as long as it's not in the beginning)
+	// underscores
+
+	// Valid variable name: num, num1, n1n2n3, _n1, n1_and_n2
+	// Invalid variable name: 1num, $num, @&!res
+
+	// camelCase  -> firstName, lastName
+	// PascalCase -> FirstName, LastName
+	// snake_case -> first_name, last_name
+	// UPPER_CASE -> FIRST_NAME, LAST_NAME
 }
