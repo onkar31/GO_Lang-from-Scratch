04dd32dceb622b4ffe53b05e8d41f3f71ef39e33
Onkar31 <onkarbhogle08@gmail.com>
2024-04-21 18:09:29 +0530

    Formatting output with placeholders

---
+++ 003.Handling-input-and-output/02_printing-output/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Onkar"
+	rollNum := 27
+	grade := "B+"
+	fmt.Printf("Hello and Welcome %s\n Your Roll number is %d\n And your grade is %s", name, rollNum, grade)  //printf function
+}
